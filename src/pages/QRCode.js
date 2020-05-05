6684e692a76b7eb4eb35236a8196d74d42784be8
DougPerson <61639363+DougPerson@users.noreply.github.com>
2020-05-05 19:16:58 -0300

    Create QRCode.js

---
+++ src/pages/QRCode.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {View,
+        Button,
+        KeyboardAvoidingView,
+        Text,
+        TextInput,
+        TouchableOpacity
+} from 'react-native';
+
+import QRCode from 'react-native-qrcode-svg';
+
+export default function QRCodeScreen({ navigation }) {
+  return (
+      <KeyboardAvoidingView behavior='padding'
+      style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <QRCode
+          value= "QRCODE"
+          color="#000"
+          backgroundColor="#fff"
+          />
+
+        <Button title="Voltar"
+         onPress={() => navigation.goBack()} />
+
+
+
+        </KeyboardAvoidingView>
+
+
+  );
+}
